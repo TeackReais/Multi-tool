a7c02698653c7dc1c4ace3dc9f15dee502eee42d
Teack Reais <Teack_Reais@outlook.com>
2016-10-02 14:12:19 +0800

    update

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -23,7 +23,7 @@ void thetimeandbanben(void);
 int main() 
 {
 	const char panfu[][26] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" }; //数组从0开始,(0-25),共26个,使用时输入(0-25)
-	const char thebiaozhunnum[][100] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100 };
+	const char thebiaozhunnum[][100] = { "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17",18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100 };
 	char str4[80]; 
 	_getcwd(str4, sizeof(str4));
 	char str[100];
@@ -1056,7 +1056,7 @@ int main()
 					thebackover();
 					continue;
 				}
-				else if (_stricmp(str, "Test") == 0)
+/*				else if (_stricmp(str, "Test") == 0)
 				{
 					int NUM1;
 					int NUM2;
@@ -1074,7 +1074,7 @@ int main()
 					printf("dingzhinum1=%d,位于=%cp,dingzhinum2=%d,位于%cp", dingzhinum[1], thep1, dingzhinum[2], thep2);
 //					FILE *fp1 = fopen(".\\test.txt","r+");
 //					fgets(fp1, 1, thetxt);
-/*					FILE*fp = NULL;
+					FILE*fp = NULL;
 					fp = fopen(SHUTDOWNCOUNTDOWN, "r");
 					if (NULL == fp)
 					{
@@ -1086,9 +1086,9 @@ int main()
 					{
 						thebackover();
 						continue;
-					}*/
+					}
 					thebackover();
-				}
+				} */
 				else if (_stricmp(str, "DAXIAOCESHI") == 0)
 				{
 					char test1[200];
